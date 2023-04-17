916c00e273b31aa5459fb98e4bec73f1febdd0a8
Alena Dubadzelava <dubodelova.minsk@gmail.com>
2023-04-17 17:05:21 +0200

    done

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,22 +1,16 @@
 package com.epam.demo;
-
+import com.epam.utils.StringUtils;
 import java.util.List;
+
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        if (args == null || args.isEmpty()) {
-            return false;
-        }
         for (String arg : args) {
-            try {
-                int num = Integer.parseInt(arg);
-                if (num < 0) {
-                    return false;
-                }
-            } catch (NumberFormatException e) {
+            if (!StringUtils.isPositiveNumber(arg)) {
                 return false;
             }
         }
         return true;
     }
 }
+
